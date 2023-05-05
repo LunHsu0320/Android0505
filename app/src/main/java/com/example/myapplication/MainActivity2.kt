5334c6799a9990cf9b3fd0a472cbd92fde2bbc03
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-05 16:57:46 +0800

    test OrderActivity

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+    fun btn_back(v: View){
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
