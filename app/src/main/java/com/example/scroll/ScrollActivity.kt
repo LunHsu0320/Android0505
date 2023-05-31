1a8d881d8ea0d4623da74a8509bd831f3512ee63
LunHsu0320 <128460048+LunHsu0320@users.noreply.github.com>
2023-05-31 11:04:41 +0800

    Android Scroll 註解

--- app/src/main/java/com/example/scroll/ScrollActivity.kt
+++ app/src/main/java/com/example/scroll/ScrollActivity.kt
@@ -20,16 +20,19 @@ class ScrollActivity : AppCompatActivity() {
 
         //畫面佈局 載入器
         var li : LayoutInflater = LayoutInflater.from(this)
-
-        //載入R.layout.student 到 ScrollView 裡 LinearLayout
+        //載入R.layout.scroll_student 到 ScrollView 裡 LinearLayout
         var ll: LinearLayout = findViewById(R.id.ll)
+        //載入器功能 inflate 載入資料 R.layout.scroll_student 到 LinearLayout
         var v:View = li.inflate(R.layout.scroll_student,ll,false)
+        //ll加入v的資料
         ll.addView(v)
 
+        // 在載入的ll視圖中查找 TextView 元素
         var tv_name:TextView = v.findViewById(R.id.tv_n)
         var tv_eng:TextView = v.findViewById(R.id.tv_e)
         var tv_math:TextView = v.findViewById(R.id.tv_m)
 
+        // 設置 TextView 的內容
         tv_name.text = r2.name
         tv_eng.text = r2.eng.toString()
         tv_math.text = r2.math.toString()
